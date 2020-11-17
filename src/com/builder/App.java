2d4136acd5ef25f4450bac2e00750396b770255c
samu <muggli.sandro@gmail.com>
2020-11-17 15:33:35 +0100

    no builder -> confusing constructor with many parameters

--- src/com/builder/App.java
+++ src/com/builder/App.java
@@ -2,31 +2,20 @@ package com.builder;
 
 public class App {
     public static void main(String[] args) {
-        Vehicle motorCycle = new Vehicle
-                .Builder(VehicleType.MOTORCYCLE)
-                    .engine(new Engine(100, 2, "unleaded"))
-                    .seats(2)
-                    .wheels(2)
-                    .build(); // creates new instance of Vehicle and returns it
 
-        System.out.println("Motorcycle created via Builder: " + motorCycle);
+        // price not relevant
+        Vehicle motorCycle = new Vehicle(VehicleType.MOTORCYCLE, 0, 2, new Engine(100, 2, "unleaded"), 2);
 
-        Vehicle bicycle = new Vehicle
-                .Builder(VehicleType.BICYCLE)
-                    .seats(1)
-                    .price(700)
-                    .wheels(2)
-                    .build();
+        System.out.println("Motorcycle created: " + motorCycle);
 
-        System.out.println("Bicycle created via Builder: " + bicycle);
+        // engine not relevant
+        Vehicle bicycle = new Vehicle(VehicleType.BICYCLE, 700, 1, null, 2);
 
-        Vehicle snowmobile = new Vehicle
-                .Builder(VehicleType.SNOWMOBILE)
-                    .seats(5)
-                    .price(1000)
-                    .engine(new Engine(130, 4, "diesel"))
-                    .build();
+        System.out.println("Bicycle created: " + bicycle);
 
-        System.out.println("Snowmobile created via Builder: " + snowmobile);
+        // wheels not relevant
+        Vehicle snowmobile = new Vehicle(VehicleType.SNOWMOBILE, 1000, 5, new Engine(130, 4, "diesel"), 0);
+
+        System.out.println("Snowmobile created: " + snowmobile);
     }
 }
